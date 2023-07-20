4c64e9c6050c71ae2b1fcd7a6f7cb84ffb99f89a
mansourlotfi <mansourlotfi@gmail.com>
2023-07-20 10:25:07 +0330

    Initialization

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,28 @@
-import Head from 'next/head';
-import { CacheProvider } from '@emotion/react';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { CssBaseline } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
-import { AuthConsumer, AuthProvider } from 'src/contexts/auth-context';
-import { useNProgress } from 'src/hooks/use-nprogress';
-import { createTheme } from 'src/theme';
-import { createEmotionCache } from 'src/utils/create-emotion-cache';
-import 'simplebar-react/dist/simplebar.min.css';
+import Head from "next/head";
+import { CacheProvider } from "@emotion/react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { AuthConsumer, AuthProvider } from "src/contexts/auth-context";
+import { useNProgress } from "src/hooks/use-nprogress";
+import { createTheme } from "src/theme";
+import createCache from "@emotion/cache";
+import rtlPlugin from "stylis-plugin-rtl";
+// import { prefixer } from "stylis";
 
-const clientSideEmotionCache = createEmotionCache();
+// import { createEmotionCache } from "src/utils/create-emotion-cache";
+import "simplebar-react/dist/simplebar.min.css";
+
+// const clientSideEmotionCache = createEmotionCache();
 
 const SplashScreen = () => null;
+// const cacheRtl = createCache({ key: "muirtl", stylisPlugins: [prefixer, rtlPlugin] });
+const cacheRtl = createCache({ key: "muirtl", stylisPlugins: [rtlPlugin] });
 
 const App = (props) => {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  // const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const { Component, pageProps } = props;
 
   useNProgress();
 
@@ -24,25 +31,18 @@ const App = (props) => {
   const theme = createTheme();
 
   return (
-    <CacheProvider value={emotionCache}>
+    <CacheProvider value={cacheRtl}>
       <Head>
-        <title>
-          Devias Kit
-        </title>
-        <meta
-          name="viewport"
-          content="initial-scale=1, width=device-width"
-        />
+        <title>Devias Kit</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <AuthProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
             <AuthConsumer>
-              {
-                (auth) => auth.isLoading
-                  ? <SplashScreen />
-                  : getLayout(<Component {...pageProps} />)
+              {(auth) =>
+                auth.isLoading ? <SplashScreen /> : getLayout(<Component {...pageProps} />)
               }
             </AuthConsumer>
           </ThemeProvider>
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,8 +1,8 @@
-import { createTheme as createMuiTheme } from '@mui/material';
-import { createPalette } from './create-palette';
-import { createComponents } from './create-components';
-import { createShadows } from './create-shadows';
-import { createTypography } from './create-typography';
+import { createTheme as createMuiTheme } from "@mui/material";
+import { createPalette } from "./create-palette";
+import { createComponents } from "./create-components";
+import { createShadows } from "./create-shadows";
+import { createTypography } from "./create-typography";
 
 export function createTheme() {
   const palette = createPalette();
@@ -11,21 +11,22 @@ export function createTheme() {
   const typography = createTypography();
 
   return createMuiTheme({
+    direction: "rtl",
     breakpoints: {
       values: {
         xs: 0,
         sm: 600,
         md: 900,
         lg: 1200,
-        xl: 1440
-      }
+        xl: 1440,
+      },
     },
     components,
     palette,
     shadows,
     shape: {
-      borderRadius: 8
+      borderRadius: 8,
     },
-    typography
+    typography,
   });
 }
