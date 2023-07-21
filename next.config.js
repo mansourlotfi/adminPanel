c13b19f66680a850595ae5bba3de57a5923f5763
mansourlotfi <mansourlotfi@gmail.com>
2023-07-21 17:13:28 +0330

    remove asset prefics for github pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,4 @@ module.exports = {
   output: "export",
   trailingSlash: true,
   reactStrictMode: true,
-  //this is for github pages only. comment it for your personal deploying method
-  assetPrefix: "https://mansourlotfi.github.io/adminPanel/", // assetPrefix requires the trailing slash
 };
