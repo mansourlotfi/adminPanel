042c1ebca5252410d4770fe5602cd0d45ff2acd2
mansourlotfi <mansourlotfi@gmail.com>
2023-07-21 16:35:45 +0330

    add public url env for deploying in github pages

--- next.config.js
+++ next.config.js
@@ -1,23 +1,5 @@
-// for github pages only
-const repo = "adminPanel";
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
-
 module.exports = {
-  images: {
-    loader: "akamai",
-    path: basePath,
-  },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   output: "export",
   trailingSlash: true,
   reactStrictMode: true,
 };
-// for github pages only
-
-// module.exports = {
-//   output: "export",
-//   trailingSlash: true,
-//   reactStrictMode: true,
-// };
