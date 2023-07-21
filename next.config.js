596d974c7eabcb33461810e63e9b7eaed03e91a1
mansourlotfi <mansourlotfi@gmail.com>
2023-07-21 17:26:47 +0330

    Add basepath and asset prefix for github pages

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   output: "export",
   trailingSlash: true,
   reactStrictMode: true,
+  //this options is for deplying in github pages , comment it for your hosting
+  basePath: "/adminPanel",
+  assetPrefix: "/adminPanel/", // assetPrefix requires the trailing slash
 };
