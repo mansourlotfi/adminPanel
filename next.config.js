77a81e5748adf3c7d17daa805f8ec6befd5c14e2
mansourlotfi <mansourlotfi@gmail.com>
2023-07-21 16:15:41 +0330

    Add new next config for github pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
+// for github pages only
+const repo = "adminPanel";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 module.exports = {
+  images: {
+    loader: "akamai",
+    path: basePath,
+  },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   output: "export",
   trailingSlash: true,
   reactStrictMode: true,
 };
+// for github pages only
+
+// module.exports = {
+//   output: "export",
+//   trailingSlash: true,
+//   reactStrictMode: true,
+// };
